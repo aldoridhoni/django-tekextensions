c42e873b4beef9bcec3e62f64d040d4a89cf24f8
Aldo Ridhoni <aldoridhoni@gmail.com>
2016-10-08 10:59:21 +0700

    Update forms.py

--- tekextensions/forms.py
+++ tekextensions/forms.py
@@ -9,6 +9,6 @@ def normalize_model_name(model_name):
 def get_model_form(model_name):
     for model in apps.get_models():
         if model.__name__ == model_name: 
-            return modelform_factory(model)
+            return modelform_factory(model, fields='__all__')
 
     raise Exception('Did not find the model %s' % model_name)
